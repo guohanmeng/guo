12ef718fa5a17448efc437bb1e6885f1e06d4d20
CarrotLiu <79093800+CarrotLiu@users.noreply.github.com>
2022-02-10 10:00:11 -0500

    Novel_Clock_H2
    Assignment3

---
+++ Novel_Clock_H2/src/abandoned.ts
@@ -0,0 +1,93 @@
+// load texture
+// let container = new PIXI.Container();
+// const load = (app: PIXI.Application) => {
+//     return new Promise<void>((resolve) => {
+//         app.loader
+//             .add('a1', 'assets/1.svg')
+//             .add('a2', 'assets/2.svg')
+//             .add('a3', 'assets/3.svg')
+//             .add('a4', 'assets/4.svg')
+//             .add('a5', 'assets/5.svg')
+//             .add('a6', 'assets/6.svg')
+//             .add('a7', 'assets/7.svg')
+//             .add('a8', 'assets/8.svg')
+//             .add('a9', 'assets/9.svg')
+//             .add('a10', 'assets/10.svg')
+//             .add('a11', 'assets/11.svg')
+//             .add('a12', 'assets/12.svg')
+           
+//             .load(() => {
+//             resolve();
+//         });
+//     });
+// };
+
+
+
+//  for(let j = 0; j < 12; j++){
+//      let sprites[j] = new PIXI.Sprite(
+//         app.loader.resources.sprites[j].texture)
+//     };
+
+
+    // let a1 = new PIXI.Sprite(
+    // app.loader.resources.a1.texture  
+    // ); 
+    // sprites.push(a1);
+
+    // let a2 = new PIXI.Sprite(
+    //     app.loader.resources.a2.texture  
+    // );
+    // sprites.push(a2);
+
+    // let a3 = new PIXI.Sprite(
+    //     app.loader.resources.a3.texture  
+    // ); 
+    // sprites.push(a3);
+
+    // let a4 = new PIXI.Sprite(
+    //     app.loader.resources.a4.texture  
+    // );
+    // sprites.push(a4);
+
+    // let a5 = new PIXI.Sprite(
+    //     app.loader.resources.a5.texture  
+    // ); 
+    // sprites.push(a5);
+
+    // let a6 = new PIXI.Sprite(
+    //         app.loader.resources.a6.texture  
+    // );
+    // sprites.push(a6);
+
+    // let a7 = new PIXI.Sprite(
+    //         app.loader.resources.a7.texture  
+    // ); 
+    // sprites.push(a7);
+
+    // let a8 = new PIXI.Sprite(
+    //         app.loader.resources.a8.texture  
+    // );
+    // sprites.push(a8);
+
+    // let a9 = new PIXI.Sprite(
+    //     app.loader.resources.a9.texture  
+    // ); 
+    // sprites.push(a9);
+
+    // let a10 = new PIXI.Sprite(
+    //         app.loader.resources.a10.texture  
+    // );
+    // sprites.push(a10);
+
+    // let a11 = new PIXI.Sprite(
+    //         app.loader.resources.a11.texture  
+    // ); 
+    // sprites.push(a11);
+
+    // let a12 = new PIXI.Sprite(
+    //         app.loader.resources.a12.texture  
+    // );
+    // sprites.push(a12);
+    
+    // console.log(sprites[0]);
---
+++ Novel_Clock_H2/src/main.ts
@@ -0,0 +1,48 @@
+// Assignment 2 - Xinyu Liu Carrot
+
+import * as PIXI from "pixi.js"
+import { PI_2, Sprite } from "pixi.js";
+import * as dat from 'dat.gui';
+
+
+const gui = new dat.GUI();
+let folder1 = gui.addFolder('My folder');
+// Add a string controller. 
+let person = {name: 'Sam'}; 
+gui.add(person, 'name');  
+// Add a number controller slider. 
+let car = {speed: 45}; 
+gui.add(person, 'age', 0, 100);
+
+// import * as Filters from 'pixi-filters'
+
+
+let buttonData = {
+    width: 200,
+    height: 100
+}
+gui.add(buttonData, "width", 0, 500);
+
+// main function
+const main = async () => {
+
+    let app = new PIXI.Application();
+
+    // Application Display
+    document.body.style.margin = '0';
+    app.renderer.view.style.position = 'absolute';
+    app.renderer.view.style.display = 'block';
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+}
+
+let button = new PIXI.Graphics();
+
+
+function onHover(){
+
+}
+
+   
+
+
+
