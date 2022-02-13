2a0ccdb47be46c1aad8f4ef4726f6fc2f92947b8
CarrotLiu <79093800+CarrotLiu@users.noreply.github.com>
2022-02-13 18:12:13 -0500

    setMonth

--- Novel_Clock_H2/src/main.ts
+++ Novel_Clock_H2/src/main.ts
@@ -2,13 +2,11 @@
 
 import * as PIXI from "pixi.js"
 
-interface clockVariable {
-	velocity: any;
-	time: number;
-	backgroundRect: PIXI.Graphics;
-	slider: PIXI.Graphics;
-	dayCircle: PIXI.Graphics;
-	secondCircle: PIXI.Graphics;
+interface flowerVar {
+	// velocity: any;
+	petal: PIXI.Graphics;
+	center: PIXI.Graphics;
+	backgroundBox: PIXI.Graphics;
 }
 
 let colorWinter: Array<number> = [0x931A25, 0xE97171, 0xF5EFEF];
@@ -16,13 +14,11 @@ let colorSpring: Array<number> = [0xEFD9D1, 0xD8AC9C, 0x999B84];
 let colorSummer: Array<number> = [0xffca00, 0x240d00, 0x648e00];
 let colorAutumn: Array<number> = [0xffd300, 0xffa100, 0xa74c00];
 
-// interface nowTime {
-//   date: number;
-//   hour: number;
-//   minute: number;
-//   second: number;
-// }
-
+const Today = () => {
+	let today = new Date();
+	today.setHours(0, 0, 0, 0);
+	return today;
+};
 // const timeNow: clockVariable = {}
 
 // get current time
@@ -46,6 +42,17 @@ console.log(getNow);
     // app.renderer.backgroundColor = 0xFFF0D9;
     app.renderer.backgroundColor = 0xFFB200;
 
+    
+    
+    let petal = new PIXI.Graphics();
+    app.stage.addChild(petal);
+
+    let center = new PIXI.Graphics();
+    app.stage.addChild(center);
+
+    let backgroundBox = new PIXI.Graphics();
+    app.stage.addChild(center);
+
     // Handle window resizing
     window.addEventListener('resize', (_e) => {
         app.renderer.resize(window.innerWidth, window.innerHeight);
@@ -53,6 +60,11 @@ console.log(getNow);
 
     document.body.appendChild(app.view);
     
+    let context: flowerVar = {
+      petal,
+      center,
+      backgroundBox
+    };
   
   }
   main();
@@ -71,8 +83,7 @@ console.log(getNow);
   //   dayCircle.y = window.innerHeight / 2 - dayCircle.height / 2;
   //   app.stage.addChild(dayCircle);
   
-  //   const secondCircle = new PIXI.Graphics();
-  //   app.stage.addChild(secondCircle);
+
   
   //   // Handle window resizing
   //   window.addEventListener('resize', (_e) => {
@@ -81,97 +92,91 @@ console.log(getNow);
   //     dayCircle.y = window.innerHeight / 2;
   //   });
   
-  //   document.body.appendChild(app.view);
+
   
   //   let time = 0;
   
-  //   let context: AppState = {
-  //     velocity: { x: 1, y: 1 },
-  //     backgroundRect,
-  //     slider,
-  //     dayCircle,
-  //     secondCircle,
-  //     time,
-  //   };
+
   
   //   app.ticker.add(update, context);
   // };
   
-  // // Cannot be an arrow function. Arrow functions cannot have a 'this' parameter.
-  // function update(this: any, delta: number) {
-  //   let currentSeconds = Math.floor(
-  //     (new Date().getTime() - TODAY().getTime()) / 1000
-  //   );
-  //   let dayProgress = currentSeconds / dayInSeconds;
-  //   let circleWidth;
+
+  function update(this: any, delta: number) {
+    let currentSeconds = Math.floor(
+      (new Date().getTime() - Today().getTime()) / 1000
+    );
+
+    let circleWidth;
+    let colorSet;
+    let pedalL, pedalW;
+    let centerS;
+
+    if (new Date().getMonth() == 11 || new Date().getMonth() == 0 || new Date().getMonth() == 1){
+      colorSet = colorWinter;
+      pedalL = 23;
+      pedalW = 20;
+      centerS = 5;
+    } else if (new Date().getMonth() == 2 || new Date().getMonth() == 3 || new Date().getMonth() == 4){
+      colorSet = colorSpring;
+      pedalL = 25;
+      pedalW = 20;
+      centerS = 3;
+    } else if (new Date().getMonth() == 5 || new Date().getMonth() == 6 || new Date().getMonth() == 7){
+      colorSet = colorSummer;
+      pedalL = 30;
+      pedalW = 15;
+      centerS = 30;
+    } else if (new Date().getMonth() == 8 || new Date().getMonth() == 9 || new Date().getMonth() == 10){
+      colorSet = colorAutumn;
+      pedalL = 30;
+      pedalW = 8;
+      centerS = 3;
+    } 
   
-  //   //Time convert
-  //   let secondProgress = new Date().getSeconds() / 60;
-  //   let minProgress = new Date().getMinutes() / 60;
+    //Time convert
+    let secondProgress = new Date().getSeconds() / 60;
+    let minProgress = new Date().getMinutes() / 60;
   
-  //   this.backgroundRect.clear();
-  //   this.backgroundRect.beginFill(0x5da1e2, 1 - dayProgress);
-  //   this.backgroundRect.drawRoundedRect(
-  //     50,
-  //     50,
-  //     window.innerWidth - 100,
-  //     window.innerHeight - 100,
-  //     30
-  //   );
-  //   this.backgroundRect.endFill();
+    this.backgroundRect.clear();
+    this.backgroundRect.beginFill(0x5da1e2, 1 - dayProgress);
+    this.backgroundRect.drawRoundedRect(
+      50,
+      50,
+      window.innerWidth - 100,
+      window.innerHeight - 100,
+      30
+    );
+    this.backgroundRect.endFill();
+    this.dayCircle.clear();
+    this.dayCircle.lineStyle(minProgress * 30, 0xffffff, 1);
+    this.dayCircle.drawCircle(0, 0, circleWidth);
+    if (window.innerWidth > window.innerHeight) {
+      this.dayCircle.x =
+        (window.innerWidth / 1.5 - this.dayCircle.width) * dayProgress +
+        (window.innerWidth / 2 -
+          window.innerWidth / 3 +
+          this.dayCircle.width / 2);
+    } else if (window.innerWidth < window.innerHeight) {
+      this.dayCircle.y =
+        (window.innerHeight / 1.5 - this.dayCircle.width) * dayProgress +
+        (window.innerHeight / 2 -
+          window.innerHeight / 3 +
+          this.dayCircle.width / 2);
+    }
   
-  //   this.slider.clear();
-  //   this.slider.beginFill(0xfafafa, 0.5);
-  //   if (window.innerWidth > window.innerHeight) {
-  //     circleWidth = window.innerWidth / (7 * 2.5);
-  //     this.slider.drawRoundedRect(
-  //       window.innerWidth / 2 - window.innerWidth / 3,
-  //       window.innerHeight / 2 - window.innerHeight / 7,
-  //       window.innerWidth / 1.5,
-  //       window.innerHeight / 3.5,
-  //       500
-  //     );
-  //   } else if (window.innerWidth < window.innerHeight) {
-  //     circleWidth = window.innerHeight / (7 * 2.5);
-  //     this.slider.drawRoundedRect(
-  //       window.innerWidth / 2 - window.innerWidth / 7,
-  //       window.innerHeight / 2 - window.innerHeight / 3,
-  //       window.innerWidth / 3.5,
-  //       window.innerHeight / 1.5,
-  //       500
-  //     );
-  //   }
-  //   this.slider.endFill();
+    this.secondCircle.clear();
+    this.secondCircle.x = this.dayCircle.x;
+    this.secondCircle.y = this.dayCircle.y;
   
-  //   this.dayCircle.clear();
-  //   this.dayCircle.lineStyle(minProgress * 30, 0xffffff, 1);
-  //   this.dayCircle.drawCircle(0, 0, circleWidth);
-  //   if (window.innerWidth > window.innerHeight) {
-  //     this.dayCircle.x =
-  //       (window.innerWidth / 1.5 - this.dayCircle.width) * dayProgress +
-  //       (window.innerWidth / 2 -
-  //         window.innerWidth / 3 +
-  //         this.dayCircle.width / 2);
-  //   } else if (window.innerWidth < window.innerHeight) {
-  //     this.dayCircle.y =
-  //       (window.innerHeight / 1.5 - this.dayCircle.width) * dayProgress +
-  //       (window.innerHeight / 2 -
-  //         window.innerHeight / 3 +
-  //         this.dayCircle.width / 2);
-  //   }
-  
-  //   this.secondCircle.clear();
-  //   this.secondCircle.x = this.dayCircle.x;
-  //   this.secondCircle.y = this.dayCircle.y;
-  
-  //   this.secondCircle.beginFill(0x4f398a, secondProgress);
-  //   this.secondCircle.drawCircle(
-  //     0,
-  //     0,
-  //     (this.dayCircle.width / 2) * secondProgress
-  //   );
-  //   this.secondCircle.endFill();
-  // }
+    this.secondCircle.beginFill(0x4f398a, secondProgress);
+    this.secondCircle.drawCircle(
+      0,
+      0,
+      (this.dayCircle.width / 2) * secondProgress
+    );
+    this.secondCircle.endFill();
+  }
   
   // main();
   
@@ -191,6 +196,9 @@ console.log(getNow);
       this.flowerSize = Math.floor(Math.random() * 3 + 1);
       this.petalColor = colors[0];
       this.centerColor = colors[1];
+    }
+
+    petal() {
 
     }
   }
